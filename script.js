3fa601e6c750029680abbb0dbc7b23c689b6bed8
Matt Garcia <mattrg1105@gmail.com>
2019-05-14 20:36:00 -0700

    added render functions

---
+++ script.js
@@ -0,0 +1,52 @@
+const data = {
+  customers: {
+    Moe: {
+      Foo: 0,
+      Bar: 0,
+      Bazz: 0
+    },
+    Larry: {
+      Foo: 0,
+      Bar: 0,
+      Bazz: 0
+    },
+    Curly: {
+      Foo: 0,
+      Bar: 0,
+      Bazz: 0
+    }
+  },
+  prizes: {
+    Foo: 1,
+    Bar: 3,
+    Bazz: 5
+  }
+};
+
+
+
+const renderCustomers = () => {
+
+const customerDiv = document.querySelector('#customers');
+
+const customers = data.customers;
+
+const html = `${Object.keys(customers).map( key => `<div> ${ key }</div>` ).join('')}`
+customerDiv.innerHTML = html;
+
+}
+
+renderCustomers();
+
+const renderPrizes = () => {
+
+  const prizeDiv = document.querySelector('#prizes');
+
+  const prizes = data.prizes;
+
+  const html = `${Object.keys(prizes).map ( key => `<div> ${ key }</div>`).join('')}`
+
+  prizeDiv.innerHTML = html;
+}
+
+renderPrizes();
